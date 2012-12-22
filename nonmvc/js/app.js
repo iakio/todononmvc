3c4d74d51885fa13310356b7d00cab6900e84d6d
ISHIDA Akio <ishida@coderium.com>
2012-12-22 20:06:01 +0900

    New todo.

--- nonmvc/js/app.js
+++ nonmvc/js/app.js
@@ -1,6 +1,36 @@
-(function( window ) {
-	'use strict';
+(function(window, $) {
+    'use strict';
 
-	// Your starting point. Enjoy the ride!
+    $(function () {
+        $('#new-todo').on('keypress', function (event) {
+            var $this = $(this),
+                todo = $(this).val().trim();
 
-})( window );
+            if (event.which !== 13) {
+                return;
+            }
+            if (todo === '') {
+                return;
+            }
+            $('#todo-list').append([
+                  '<li>'
+                ,     '<div class="view">'
+                ,         '<input class="toggle" type="checkbox">'
+                ,         '<label>' + todo + '</label>'
+                ,         '<button class="destroy"></button>'
+                ,     '</div>'
+                ,     '<input class="edit" value="' + todo + '">'
+                , '</li>'
+            ].join(''));
+
+            $('#todo-count').html(
+                $('#todo-count').html().replace(/\d+/, function (n) {
+                    return +n + 1;
+                })
+            );
+
+            $this.val('');
+        });
+
+    });
+})(window, jQuery);
