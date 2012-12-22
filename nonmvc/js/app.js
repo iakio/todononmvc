ddae9f5bd6cef491558b3fe3613f61b54a0f9be1
ISHIDA Akio <ishida@coderium.com>
2012-12-22 20:48:28 +0900

    mark item as completed.

--- nonmvc/js/app.js
+++ nonmvc/js/app.js
@@ -32,5 +32,37 @@
             $this.val('');
         });
 
+        $('#main').on('change', 'input.toggle', function () {
+            var $this = $(this),
+                done = $this.is(':checked');
+
+            if (done) {
+                $this.parents('li').addClass('completed');
+                $('#clear-completed').html(
+                    $('#clear-completed').html().replace(/\d+/, function (n) { return +n + 1; })
+                ).show();
+                $('#todo-count').html(
+                    $('#todo-count').html().replace(/\d+/, function (n) {
+                        return n - 1;
+                    })
+                );
+            } else {
+                $this.parents('li').removeClass('completed');
+                $('#clear-completed').html(
+                    $('#clear-completed').html().replace(/\d+/, function (n) {
+                        if (n === '1') {
+                            $('#clear-completed').hide();
+                        }
+                        return n - 1;
+                    })
+                );
+                $('#todo-count').html(
+                    $('#todo-count').html().replace(/\d+/, function (n) {
+                        return +n + 1;
+                    })
+                );
+            }
+        });
+
     });
 })(window, jQuery);
